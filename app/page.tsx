086fe2dc13cc5c173490f0b9670b24054b8ed5d4
sameer-jafar-05 <sameer.jafar.05@gmail.com>
2025-08-30 15:27:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import Navbar from "../components/layout/Navbar";
-import Footer from "../components/layout/Footer";
-import Hero from "../components/sections/Hero";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main style={{ fontFamily: "Arial, sans-serif" }}>
+    <>
       <Navbar />
       <Hero />
+      <About />
       <Footer />
-    </main>
+    </>
   );
 }
